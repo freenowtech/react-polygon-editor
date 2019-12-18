002074de8fdec985cc403c44938f6c0b594d647a
Askan Hesse <askan.hesse@free-now.com>
2019-12-18 11:49:32 +0100

    refactor: stronger typing for isValidPolygon method

--- src/PolygonDraw/validators.ts
+++ src/PolygonDraw/validators.ts
@@ -1,4 +1,4 @@
 import { Coordinate } from 'types';
 import { isPolygonClosed } from '../helpers';
 
-export const isValidPolygon = (coordinates: Coordinate[]) => coordinates && coordinates.length > 3 && isPolygonClosed(coordinates);
+export const isValidPolygon = (coordinates: Coordinate[]): boolean => !!coordinates && coordinates.length > 3 && isPolygonClosed(coordinates);
