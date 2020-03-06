7ec0b9779e86a952103bf831a1a39e44cc8b4f03
Luka Hartwig <l.hartwig@mytaxi.com>
2020-03-06 15:04:42 +0100

    feat: detect import format (currently only GeoJSON)

--- src/conversion/ImportPolygonForm.tsx
+++ src/conversion/ImportPolygonForm.tsx
@@ -1,13 +1,22 @@
-import React, { FormEventHandler } from 'react';
+import React, { FormEventHandler, useState } from 'react';
 import styled from 'styled-components';
 
 import { Coordinate } from 'types';
 
+import { Button } from '../common/components/Button';
+import { ButtonGroup } from '../common/components/ButtonGroup';
 import { Headline } from '../common/components/Headline';
+import { Textarea } from '../common/components/Textarea';
+import { useDismiss } from '../common/components/Modal';
+import { useDeserialize } from './useDeserialize';
 
 const Form = styled.form`
     display: flex;
     flex-direction: column;
+`;
+
+const StyledTextarea = styled(Textarea)`
+    margin-bottom: 24px;
 `;
 
 interface Props {
@@ -15,16 +24,31 @@ interface Props {
 }
 
 export const ImportPolygonForm: React.FC<Props> = ({ onSubmit }) => {
+    const dismiss = useDismiss();
+    const [text, setText] = useState('');
+    const deserialized = useDeserialize(text);
+
     const handleOnSubmit: FormEventHandler = e => {
         e.preventDefault();
 
-        // TODO pass real coordinates
-        onSubmit([]);
+        if (deserialized.valid) {
+            onSubmit(deserialized.coordinates);
+            dismiss();
+        }
     };
 
     return (
         <Form onSubmit={handleOnSubmit}>
             <Headline>Import Polygon</Headline>
+            <StyledTextarea placeholder="Insert code here" value={text} onChange={e => setText(e.target.value)} />
+            <ButtonGroup>
+                <Button type="submit" disabled={!deserialized.valid}>
+                    Import
+                </Button>
+                <Button type="button" secondary onClick={dismiss}>
+                    Close
+                </Button>
+            </ButtonGroup>
         </Form>
     );
 };
