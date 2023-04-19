63e6562ad2c038f388d553edfa1bce28d21ab0f8
Inga Schünemann <inga.schunemann@mytaxi.com>
2023-04-19 12:43:43 +0200

    test: adapt ActionBar test using userEvent

--- src/ActionBar/ActionBar.test.tsx
+++ src/ActionBar/ActionBar.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, fireEvent, cleanup, screen } from '@testing-library/react';
+import { render, cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { LABELS } from '../constants';
 import { ActionBar, Props } from './ActionBar';
@@ -26,16 +27,28 @@ describe('ActionBar', () => {
 
         it('should only render the action buttons', () => {
             expect(screen.getByText(LABELS.FOCUS)).toBeTruthy();
-            expect(screen.queryByText(LABELS.EXPORT)).toBeTruthy();
-            expect(screen.queryByText(LABELS.IMPORT)).toBeTruthy();
+            expect(screen.getByText(LABELS.EXPORT)).toBeTruthy();
+            expect(screen.getByText(LABELS.IMPORT)).toBeTruthy();
             expect(screen.queryByText(LABELS.DELETE)).toBeNull();
             expect(screen.queryByText(LABELS.PEN)).toBeNull();
         });
 
-        it('should trigger the onFocus callback when user click the focus button', () => {
-            fireEvent.click(screen.getByText(LABELS.FOCUS));
+        it('should trigger the onFocus callback when user click the focus button', async () => {
+            await userEvent.click(screen.getByText(LABELS.FOCUS));
 
             expect(props.onFocus).toHaveBeenCalled();
+        });
+
+        it('should trigger the onExport callback when the user clicks the export button', async () => {
+            await userEvent.click(screen.getByText(LABELS.EXPORT));
+
+            expect(props.onExport).toHaveBeenCalled();
+        });
+
+        it('should trigger the onImport callback when the user clicks the import button', async () => {
+            await userEvent.click(screen.getByText(LABELS.IMPORT));
+
+            expect(props.onImport).toHaveBeenCalled();
         });
     });
 
@@ -48,22 +61,34 @@ describe('ActionBar', () => {
             expect(screen.getByText(LABELS.PEN)).toBeTruthy();
         });
 
-        it('should trigger the onFocus callback when user click the focus button', () => {
-            fireEvent.click(screen.getByText(LABELS.FOCUS));
+        it('should trigger the onFocus callback when user click the focus button', async () => {
+            await userEvent.click(screen.getByText(LABELS.FOCUS));
 
             expect(props.onFocus).toHaveBeenCalled();
         });
 
-        it('should trigger the onDelete callback when user click the focus button', () => {
-            fireEvent.click(screen.getByText(LABELS.DELETE));
+        it('should trigger the onDelete callback when user click the focus button', async () => {
+            await userEvent.click(screen.getByText(LABELS.DELETE));
 
             expect(props.onDelete).toHaveBeenCalled();
         });
 
-        it('should trigger the onEnableVectorMode callback when user click the focus button', () => {
-            fireEvent.click(screen.getByText(LABELS.PEN));
+        it('should trigger the onEnableVectorMode callback when user click the focus button', async () => {
+            await userEvent.click(screen.getByText(LABELS.PEN));
 
             expect(props.onEnableVectorMode).toHaveBeenCalled();
+        });
+
+        it('should trigger the onExport callback when the user clicks the export button', async () => {
+            await userEvent.click(screen.getByText(LABELS.EXPORT));
+
+            expect(props.onExport).toHaveBeenCalled();
+        });
+
+        it('should trigger the onImport callback when the user clicks the import button', async () => {
+            await userEvent.click(screen.getByText(LABELS.IMPORT));
+
+            expect(props.onImport).toHaveBeenCalled();
         });
     });
 });
