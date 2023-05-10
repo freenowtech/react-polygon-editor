b9b2b197a89e62c987422c3ebd6aa0fd0d94ca4c
Inga Schünemann <inga.schunemann@mytaxi.com>
2023-05-10 07:15:53 +0200

    Merge branch 'refactor/migrate-vertex-components-to-fc' into setup-eslint

--- src/PolygonDraw/PolygonVertex.tsx
+++ src/PolygonDraw/PolygonVertex.tsx
@@ -1,7 +1,5 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import 'leaflet-path-drag';
-import React from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
 import { CircleMarker, LatLng } from 'leaflet';
 import { CircleMarker as ReactLeafletCircleMarker } from 'react-leaflet';
 
@@ -21,93 +19,99 @@ export interface Props {
     onDragEnd: () => void;
 }
 
-export interface State {
-    isHovered: boolean;
-    isDragged: boolean;
-    latLng: LatLng;
-}
+export const PolygonVertex: FC<Props> = ({
+    coordinate,
+    isSelected,
+    index,
+    onClick,
+    onDrag,
+    onDragEnd,
+    onDragStart,
+}) => {
+    const [isHovered, setIsHovered] = useState(false);
+    const [isDragged, setIsDragged] = useState(false);
+    const [latLng, setLatLng] = useState(new LatLng(0, 0));
 
-export class PolygonVertex extends React.Component<Props, State> {
-    private circleMarkerElement: CircleMarker;
-    state = {
-        isHovered: false,
-        isDragged: false,
-        latLng: new LatLng(0, 0),
-    };
+    let circleMarkerElement: CircleMarker;
 
-    static getDerivedStateFromProps = (props: Props, state: State) => {
-        return state.isDragged ? state : { ...state, latLng: createLeafletLatLngFromCoordinate(props.coordinate) };
-    };
-
-    componentDidMount() {
-        if (this.circleMarkerElement) {
-            this.circleMarkerElement.on('dragstart', this.handleDragStart);
-            this.circleMarkerElement.on('drag', this.handleDrag);
-            this.circleMarkerElement.on('dragend', this.handleDragEnd);
+    useEffect(() => {
+        if (!isDragged && !!coordinate) {
+            setLatLng(createLeafletLatLngFromCoordinate(coordinate));
         }
-    }
+    }, [isDragged, coordinate]);
 
-    componentWillUnmount() {
-        if (this.circleMarkerElement) {
-            this.circleMarkerElement.off('dragstart', this.handleDragStart);
-            this.circleMarkerElement.off('drag', this.handleDrag);
-            this.circleMarkerElement.off('dragend', this.handleDragEnd);
-        }
-    }
+    const handleDragStart = useCallback(
+        (event: DragEvent) => {
+            onDragStart(event.target.getLatLng(), index);
+            setIsDragged(true);
+        },
+        [index, onDragStart]
+    );
 
-    // eslint-disable-next-line
-    setCircleMarkerRef = (ref: any) => {
+    const handleDrag = useCallback(
+        (event: DragEvent) => {
+            if (isDragged) {
+                onDrag(event.target.getLatLng());
+            }
+        },
+        [isDragged, onDrag]
+    );
+
+    const handleDragEnd = useCallback(() => {
+        onDragEnd();
+        setIsDragged(false);
+    }, [onDragEnd]);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const setCircleMarkerRef = (ref: any) => {
         if (ref) {
-            this.circleMarkerElement = ref;
+            circleMarkerElement = ref;
         }
     };
 
-    handleClick = () => this.props.onClick(this.props.index);
-    handleMouseOver = () => this.setState({ isHovered: true });
-    handleMouseOut = () => this.setState({ isHovered: false });
-
-    handleDragStart = (event: DragEvent) => {
-        this.props.onDragStart(event.target.getLatLng(), this.props.index);
-        this.setState({ isDragged: true });
-    };
-
-    handleDrag = (event: DragEvent) => {
-        if (this.state.isDragged) {
-            this.props.onDrag(event.target.getLatLng());
+    useEffect(() => {
+        if (circleMarkerElement) {
+            circleMarkerElement.on('dragstart', handleDragStart);
+            circleMarkerElement.on('drag', handleDrag);
+            circleMarkerElement.on('dragend', handleDragEnd);
         }
-    };
+        return () => {
+            if (circleMarkerElement) {
+                circleMarkerElement.off('dragstart', handleDragStart);
+                circleMarkerElement.off('drag', handleDrag);
+                circleMarkerElement.off('dragend', handleDragEnd);
+            }
+        };
+        // circleMarkerElement cannot be set as dependency due to assignment issues
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [handleDrag, handleDragEnd, handleDragStart]);
 
-    handleDragEnd = () => {
-        this.props.onDragEnd();
-        this.setState({ isDragged: false });
-    };
+    const handleClick = () => onClick(index);
+    const handleMouseOver = () => setIsHovered(true);
+    const handleMouseOut = () => setIsHovered(false);
 
-    render() {
-        const { isSelected } = this.props;
-        const { isHovered, latLng } = this.state;
-        const selectedOrHovered = isSelected || isHovered;
-        const hoveredAndNotSelected = isHovered && !isSelected;
+    const selectedOrHovered = isSelected || isHovered;
+    const hoveredAndNotSelected = isHovered && !isSelected;
 
-        return (
-            <ReactLeafletCircleMarker
-                aria-label="Polygon"
-                ref={this.setCircleMarkerRef}
-                fillColor={MAP.VERTEX_FILL_COLOR}
-                fillOpacity={1}
-                color={MAP.POLYGON_ACTIVE_COLOR}
-                opacity={hoveredAndNotSelected ? 0.6 : 1}
-                weight={selectedOrHovered ? 4 : 1}
-                radius={selectedOrHovered ? 6 : 4}
-                center={latLng}
-                eventHandlers={{
-                    click: this.handleClick,
-                    mouseover: this.handleMouseOver,
-                    mouseout: this.handleMouseOut,
-                }}
-                bubblingMouseEvents={false}
-                // @ts-ignore
-                draggable
-            />
-        );
-    }
-}
+    return (
+        <ReactLeafletCircleMarker
+            aria-label="Polygon"
+            ref={setCircleMarkerRef}
+            fillColor={MAP.VERTEX_FILL_COLOR}
+            fillOpacity={1}
+            color={MAP.POLYGON_ACTIVE_COLOR}
+            opacity={hoveredAndNotSelected ? 0.6 : 1}
+            weight={selectedOrHovered ? 4 : 1}
+            radius={selectedOrHovered ? 6 : 4}
+            center={latLng}
+            eventHandlers={{
+                click: handleClick,
+                mouseover: handleMouseOver,
+                mouseout: handleMouseOut,
+            }}
+            bubblingMouseEvents={false}
+            // @ts-ignore
+            draggable
+        />
+    );
+};
